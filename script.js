6ebed26717458038262556dca3d3924477d3cba8
meghanathul <92531334+meghanathul@users.noreply.github.com>
2022-03-29 13:44:13 +0530

    js8

---
+++ script.js
@@ -0,0 +1,74 @@
+// let mystr='Meghana'
+// const res=mystr.length
+// console.log(res)
+
+
+// chaAt --if we wont to find character
+// let mystr='Meghana'
+// const res=mystr.charAt(3)
+// console.log(res)
+
+//indexOf
+// let mystr='Meghana'
+// const res=mystr.indexOf('a')
+// console.log(res)
+
+//split --used to convert string to an array and split returns the new array,and dose note change the original array
+// let mystr='Meghana'
+// const res=mystr.split('')
+// console.log(res)
+
+//reverse  ----we need to use split
+// let mystr='Meghana'
+// const res=mystr.split('').reverse().join('');
+// console.log(res)
+
+//trim---is used to removed wite spaces
+// let mystr='Me    gh        ana'
+// console.log(mystr.length)
+// const res=mystr.trim()
+// console.log(res)
+// console.log(res.length)
+
+
+//slice -------use to extract data
+// let mystr=['Meghana','hello','hii','good']
+// const res=mystr.slice(1,8)
+// console.log(res)
+
+
+//splice  ------(last is counted)exact and add element and it will modify
+// let mystr=['Meghana','hello','hii','good']
+// // const res=mystr.splice(1,3)
+// const res=mystr.splice(1,3,'ddddddddddddd')
+// // console.log(res)
+// console.log(mystr)
+
+
+//concatination
+// let mystr='Meghana'
+// let mystr1='hello'
+// const res=mystr.concat(mystr1)
+// console.log(res) 
+
+
+//replace
+// let mystr='Welcome'
+// const res=mystr.replace('Welcome','goodby')
+// console.log(res)
+
+
+//replaceAll
+// let mystr='Meghana is studend, is in amravati,is in maharashtra'
+// const res=mystr.replaceAll('is','was')
+// console.log(res)
+
+//lowerrecase
+// let mystr='MEGHANA'
+// const res=mystr.toLowerCase()
+// console.log(res)
+
+//uppercase
+let mystr='MEGHANA'
+const res=mystr.toUpperCase()
+console.log(res)
